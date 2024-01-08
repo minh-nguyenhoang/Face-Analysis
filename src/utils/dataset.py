dc020f12a6c986a4ac3143ffb21950a8c788d69f
MinhNH <hoangminhn4@gmail.com>
2024-01-08 22:38:50 +0700

    add infer

--- src/utils/dataset.py
+++ src/utils/dataset.py
@@ -100,46 +100,4 @@ class PixtaDataset(Dataset):
         
         return img, age, gender, masked, emotion, race, skin
     
-if __name__ == "__main__":
-    dts = PixtaDataset(root='Face-Analysis/src/data/cropped_data',
-                       csv_file='Face-Analysis/sfrom torch.utils.data import Dataset, DataLoader
-import albumentations
-import torch
-from .data_process.letterbox import letterbox
-import pandas as pd
-import os
-import cv2
 
-
-class PixtaDataset(Dataset):
-    def __init__(self, 
-                 root = '.',
-                 csv_file = '',
-                 img_size = (224,224),
-                 transform = None,) -> None:
-        super().__init__()
-
-        self.root = root
-        self.img_size = img_size
-
-        try:
-            self.metadata = pd.read_csv(os.path.join(root, csv_file))
-            self.metadata_path = os.path.join(root, csv_file)
-        except:
-            try:
-                self.metadata = pd.read_csv(csv_file)
-                self.metadata_path = csv_file
-            except Exception as e:
-                raise e
-            
-
-        self.transform = transform
-
-    def __len_(self):
-        return len(self.metadata)
-    
-    def __getitem__(self, index):
-
-        
-
-        return 
