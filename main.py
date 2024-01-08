8ee74c021d30b40b5276b63f49bd597b56f6e2da
Viet Dung Nguyen <71597672+kpup1710@users.noreply.github.com>
2024-01-08 21:26:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from torch.utils.data import DataLoader
 from src.utils.dataset import PixtaDataset
 from src.utils.loss import multi_task_loss
 import timm
+from tqdm import tqdm
 
 def checkpoint_filter_fn(state_dict, model):
     """ Remap FB checkpoints -> timm """
