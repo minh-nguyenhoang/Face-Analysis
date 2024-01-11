730cb85a8b6082a56ea8e8d61de7bd5fff593b97
MinhNH <hoangminhn4@gmail.com>
2024-01-12 00:56:28 +0700

    add data augmentation

--- src/utils/dataset.py
+++ src/utils/dataset.py
@@ -35,6 +35,7 @@ class PixtaDataset(Dataset):
         self.train_transform = transforms.Compose([
             transforms.RandomApply([transforms.ColorJitter(0.25, 0.25, 0.02, 0.02)],p = 0.5),
                                             transforms.RandomApply([transforms.RandomAffine(5, (0.1,0.1), (1.0,1.25))], p=0.2),
+                                            cutout(),
                                             RandomGammaCorrection(),
                                             transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5)),
                                             ])
@@ -107,6 +108,8 @@ class PixtaDataset(Dataset):
         
         return img, age, gender, masked, emotion, race, skin
     
+import random 
+import torchvision.transforms.functional as TF
 class RandomGammaCorrection(object):
     '''
     Apply Gamma Correction to the images
@@ -129,3 +132,37 @@ class RandomGammaCorrection(object):
         
         else:
             return TF.adjust_gamma(image, self.gamma, gain=1)
+        
+def cutout(mask_size= 30, p= 0.5, cutout_inside= True, mask_color=(0, 0, 0)):
+    mask_size_half = mask_size // 2
+    offset = 1 if mask_size % 2 == 0 else 0
+
+    def _cutout(image):
+        image = np.asarray(image).copy()
+
+        if np.random.random() > p:
+            return image
+
+        h, w = image.shape[:2]
+
+        if cutout_inside:
+            cxmin, cxmax = mask_size_half, w + offset - mask_size_half
+            cymin, cymax = mask_size_half, h + offset - mask_size_half
+        else:
+            cxmin, cxmax = 0, w + offset
+            cymin, cymax = 0, h + offset
+
+        cx = np.random.randint(cxmin, cxmax)
+        cy = np.random.randint(cymin, cymax)
+        xmin = cx - mask_size_half
+        ymin = cy - mask_size_half
+        xmax = xmin + mask_size
+        ymax = ymin + mask_size
+        xmin = max(0, xmin)
+        ymin = max(0, ymin)
+        xmax = min(w, xmax)
+        ymax = min(h, ymax)
+        image[ymin:ymax, xmin:xmax] = mask_color
+        return image
+
+    return _cutout
