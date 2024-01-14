d3281bdc7c7977176d60d7f8aaeb22b299855d1c
Viet Dung <haconga1903@gmail.com>
2024-01-14 22:46:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ if __name__ == "__main__":
   
   backbone = timm.create_model('convnext_base.fb_in22k_ft_in1k', pretrained=True)
   # checkpoint = torch.load(f'/kaggle/input/baseline-checkpoint/{args.ckpt}')
-  checkpoint = torch.load(f'args.ckpt')
+  checkpoint = torch.load(args.ckpt)
 # #   print(checkpoint['teacher'].keys())
   new_checkpoint = checkpoint_dino_filter(checkpoint['student'],backbone)
 # #   print(new_checkpoint.keys())
