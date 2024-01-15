51486d4ca783414ac2c619d20c7d26ebe62180aa
Dung <nguyenvietd67@gmail.com>
2024-01-15 23:55:18 +0700

    change head

--- src/models/bionet.py
+++ src/models/bionet.py
@@ -82,7 +82,7 @@ class BioNet(nn.Module):
             nn.Linear(128, 4)
         )
 
-        self.skin_weights = nn.parameter(torch.Tensor(0.5))
+        self.skin_weights = nn.Parameter(torch.Tensor(0.5))
 
     def forward(self, x):
         feat = self.vcn(x)
