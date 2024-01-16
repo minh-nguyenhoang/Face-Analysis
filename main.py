ebc029dbaa5a7ef486fac2b984374b6c6d678090
MinhNH <hoangminhn4@gmail.com>
2024-01-16 21:34:16 +0700

    fix bug

--- main.py
+++ main.py
@@ -114,7 +114,7 @@ if __name__ == "__main__":
 
   loss_func = multi_task_loss(device=device)
 
-  model = BioNet(backbone, 1024, 512, fine_tune=args.finetune)
+  model = BioNet(backbone, 1024, 512, n_groups= 32, fine_tune=args.finetune)
   model.to(device)
   if args.path != "None":
     try:
