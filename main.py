178a28e210539fdcf1f1b8c566574a36ef3ca7f8
MinhNH <hoangminhn4@gmail.com>
2024-01-19 23:49:48 +0700

    add bug

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ if __name__ == "__main__":
   checkpoint = torch.load(f'/kaggle/input/baseline-checkpoint/{args.ckpt}')
 #   print(checkpoint['teacher'].keys())
   new_checkpoint = checkpoint_dino_filter(checkpoint['student'],backbone)
-#   print(new_checkpoint.keys())
+  print(new_checkpoint.keys())
   backbone.load_state_dict(new_checkpoint, strict=True)
 # #   print(backbone)
   backbone.head = nn.Identity()
